630687d8f608a8a476bda5e33ca1e01e5980013c
Manuel Robert <manuelrobz@outlook.com>
2021-03-01 21:37:27 +0530

    Nearest Greater to Left Using Stack

---
+++ Nearest Greater Left.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+void NEL_STACK(int arr[], int size)
+{
+    stack<int> s;
+    vector<int> v;
+    for (int i = 0; i < size; i++)
+    {
+        if (s.size() == 0)
+        {
+            v.push_back(-1);
+        }
+        else if (s.size() > 0 && s.top() > arr[i])
+        {
+            v.push_back(s.top());
+        }
+        else if (s.size() > 0 && s.top() <= arr[i])
+        {
+            while (s.size() > 0 && s.top() <= arr[i])
+            {
+                s.pop();
+            }
+            if (s.size() == 0)
+            {
+                v.push_back(-1);
+            }
+            else
+            {
+                v.push_back(s.top());
+            }
+        }
+        s.push(arr[i]);
+    }
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << endl;
+}
+
+int main()
+{
+    int arr[] = {1, 3, 2, 4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    NEL_STACK(arr, size);
+    return 0;
+}
